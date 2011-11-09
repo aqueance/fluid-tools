33f54c66fcce325f2c2a7b754303ea237c784898
Tibor Adam Varga <tibor.adam.varga@gmail.com>
2011-11-09 20:33:29 +0200

    Javadoc updates and related refactoring - work in progress.

--- composition/composition-container-support-api/src/main/java/org/fluidity/composition/DependencyGraph.java
+++ composition/composition-container-support-api/src/main/java/org/fluidity/composition/DependencyGraph.java
@@ -22,19 +22,27 @@ import org.fluidity.composition.spi.ComponentResolutionObserver;
 import org.fluidity.composition.spi.DependencyPath;
 
 /**
- * A dependency graph of components and component groups. The graph is backed by static and dynamic dependencies between components. The graph can be traversed
- * to get a component or component group instance and during that traversal the actual dependency graph, starting at the sought component or component group,
- * is realized.
+ * A dependency graph of components and component groups. This is an internal interface used by dependency injection container implementations.
+ * <p/>
+ * The graph is backed by static and dynamic dependencies between components. Static dependencies are those that can be discovered without instantiating
+ * components while dynamic dependencies are those that require component instantiation.
+ * <p/>
+ * The graph allows the resolution of a component or component group interface to a component class or component classes, respectively, and, while doing so,
+ * the traversal of the dependency graph under that component or component group to collect information about the dependency graph(s).
+ * <p/>
+ * This interface is implemented together with the {@link DependencyGraph.Traversal} interface, the implementation of which holds all state concerning the
+ * particular graph traversal, and these together allow the implementation of a third interface, {@link ComponentResolutionObserver}, to act like a visitor by
+ * getting its callback methods invoked at certain events during graph traversal.
  *
  * @author Tibor Varga
  */
 public interface DependencyGraph {
 
     /**
-     * Resolves a component using the given traversal in the given context.
+     * Resolves, without instantiating it, a component using the given traversal in the given context.
      *
      * @param api       the component interface.
-     * @param context   the component context at the point of resolution.
+     * @param context   the component context at the node at which the resolution starts.
      * @param traversal the graph traversal to use.
      *
      * @return the resolved component or <code>null</code> if none could be resolved.
@@ -42,10 +50,10 @@ public interface DependencyGraph {
     Node resolveComponent(Class<?> api, ContextDefinition context, Traversal traversal);
 
     /**
-     * Resolves a component group with the given traversal in the given context.
+     * Resolves, without instantiating its members, a component group with the given traversal in the given context.
      *
      * @param api       the group interface.
-     * @param context   the component context at the point of resolution.
+     * @param context   the component context at the node at which the resolution starts.
      * @param traversal the graph traversal to use.
      *
      * @return the resolved component group or <code>null</code> if none could be resolved.
@@ -53,27 +61,28 @@ public interface DependencyGraph {
     Node resolveGroup(Class<?> api, ContextDefinition context, Traversal traversal);
 
     /**
-     * A dynamic node in the graph. Such nodes are created during a particular traversal of the graph from a given component or component group.
+     * A node in the graph. Nodes are created during traversal of a {@link DependencyGraph} and hold information about the node that enables proper component
+     * instantiation at the node.
      */
     interface Node {
 
         /**
-         * The actual component class at this node, or in case of circular dependency, the dependency type.
+         * The actual component class at this node, or in case of circular dependency, the class or interface being depended on.
          *
-         * @return the actual component class at this node, or in case of circular dependency, the dependency type.
+         * @return the actual component class at this node, or in case of circular dependency, the class or interface being depended on.
          */
         Class<?> type();
 
         /**
          * Creates and returns the component or component group instance at this node. This may result in further graph traversal, as in case a constructor
          * dynamically resolves a component, as well as changes to the traversal up to this point in case of a circular dependency that cannot be replaced with
-         * a proxy, due to the reference not being made to an interface.
+         * a proxy due to depending on a class rather than an interface.
          *
-         * @param traversal the graph traversal to notify about instantiation.
+         * @param traversal the current graph traversal.
          *
          * @return the instance at this node.
          */
-        Object instance(final Traversal traversal);
+        Object instance(Traversal traversal);
 
         /**
          * The component context at this node. Calculating this value requires that the path leading up to this node has already been resolved.
@@ -83,7 +92,7 @@ public interface DependencyGraph {
         ComponentContext context();
 
         /**
-         * A reference to a node that can be resolved.
+         * A resolvable reference to a node.
          */
         interface Reference {
 
@@ -104,10 +113,10 @@ public interface DependencyGraph {
             /**
              * Resolves the node using the given traversal and context.
              *
-             * @param traversal the graph traversal that maintains the dependency path.
+             * @param traversal the current graph traversal.
              * @param context   the component context at this node.
              *
-             * @return the resolved the node using the given traversal and context.
+             * @return the resolved node.
              */
             Node resolve(Traversal traversal, ContextDefinition context);
         }
@@ -147,16 +156,20 @@ public interface DependencyGraph {
     }
 
     /**
-     * A graph traversal that maintains a traversal path, calculated graph nodes and component contexts along the path and handles circular references.
+     * A graph traversal that maintains state during traversal such as current path, nodes and component contexts along the path, and handles circular
+     * references.
      */
     interface Traversal {
 
         /**
          * Follows a dependency. This is an indirectly recursive method as during resolution of a node further invocations of this method are expected and thus
-         * one single instance is able to maintain a path of invocations.
+         * one single instance is able to maintain a path of invocations. The implementation, if co-operates with a {@link DependencyGraph.Traversal.Strategy},
+         * must invoke the strategy object's {@link DependencyGraph.Traversal.Strategy#advance(DependencyGraph, ContextDefinition, DependencyGraph.Traversal,
+         * boolean, DependencyGraph.Traversal.Trail) advance()} method to actually advance on the dependency path by providing a {@link
+         * DependencyGraph.Traversal.Trail} object to do the advancing.
          *
          * @param graph     the graph in which this dependency is to be followed.
-         * @param context   the component context at the point of dependence.
+         * @param context   the component context at the point of dependency.
          * @param reference the node reference that can resolve the graph node that the dependency leads to.
          *
          * @return the resolved node.
@@ -181,11 +194,11 @@ public interface DependencyGraph {
         void instantiating(Class<?> type);
 
         /**
-         * Notifies the traversal's observer, if any, about the instantiation of the given type.
+         * Notifies the traversal's observer, if any, about the instantiation of the given type. <b>Note:</b> the receiver must not call any method on the
+         * instantiated object.
          *
          * @param type      the type just instantiated.
-         *
-         * @param component the component that has just been instantiated. The receiver must not call any method on it.
+         * @param component the component that has just been instantiated.
          */
         void instantiated(Class<?> type, Object component);
 
@@ -199,26 +212,32 @@ public interface DependencyGraph {
         /**
          * Notifies the observer registered with the receiver that a dependency is being resolved. See {@link ComponentResolutionObserver} for details.
          *
-         * @param declaringType        see {@link ComponentResolutionObserver}.
-         * @param dependencyType       see {@link ComponentResolutionObserver}.
-         * @param typeAnnotations      see {@link ComponentResolutionObserver}.
-         * @param referenceAnnotations see {@link ComponentResolutionObserver}.
+         * @param declaringType        see {@link ComponentResolutionObserver#resolving(Class, Class, Class, Annotation[], Annotation[])
+         *                             ComponentResolutionObserver}.
+         * @param dependencyType       see {@link ComponentResolutionObserver#resolving(Class, Class, Class, Annotation[], Annotation[])
+         *                             ComponentResolutionObserver}.
+         * @param typeAnnotations      see {@link ComponentResolutionObserver#resolving(Class, Class, Class, Annotation[], Annotation[])
+         *                             ComponentResolutionObserver}.
+         * @param referenceAnnotations see {@link ComponentResolutionObserver#resolving(Class, Class, Class, Annotation[], Annotation[])
+         *                             ComponentResolutionObserver}.
          */
         void resolving(Class<?> declaringType, Class<?> dependencyType, Annotation[] typeAnnotations, Annotation[] referenceAnnotations);
 
         /**
-         * A path traversal strategy. The strategy is consulted before advancing on a dependency path.
+         * A dependency graph traversal strategy. The strategy is consulted before advancing on a dependency path. This is an SPI that allows a suitable
+         * implementation to replace a repeating node, i.e., circular reference, with a delegation chain. This is currently not implemented.
          */
         @ServiceProvider
         interface Strategy {
 
             /**
-             * Advances on a dependency path. The strategy may return an unrelated Node or may invoke the given trail object to advance on the dependency path.
+             * Advances on a dependency path. The strategy may return an unrelated <code>Node</code> or may invoke the given trail object to advance on the
+             * dependency path.
              *
-             * @param graph     the graph in which the dependency path is currently followed.
+             * @param graph     the graph in which the dependency path is currently being followed.
              * @param context   the component context at the next node in the path.
              * @param traversal the traversal object that maintains the dependency path.
-             * @param repeating <code>true</code> if this node is a repetition of a previous node, as defined by the dependency API and the context.
+             * @param repeating <code>true</code> if this node is a repetition of a previous node, as defined by the dependency interface and the context.
              * @param trail     the dependency trail.
              *
              * @return the next node on the dependency path.
