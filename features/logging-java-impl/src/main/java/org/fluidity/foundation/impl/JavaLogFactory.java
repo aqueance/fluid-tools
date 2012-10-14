8a3f3cab91b77f34f6ec6477d14849a3faf2dffd
Tibor Adam Varga <tibor.adam.varga@gmail.com>
2012-10-14 23:13:37 +0300

    Added support for the JUL framework.

---
+++ features/logging-java-impl/src/main/java/org/fluidity/foundation/impl/JavaLogFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2006-2012 Tibor Adam Varga (tibor.adam.varga on gmail)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fluidity.foundation.impl;
+
+import java.util.logging.LogManager;
+
+import org.fluidity.composition.Component;
+import org.fluidity.composition.ServiceProvider;
+import org.fluidity.foundation.Log;
+import org.fluidity.foundation.spi.LogFactory;
+
+/**
+ * Log factory backed by <code>java.util.logging</code>. The log levels are mapped as follows:<ul>
+ * <li>{@linkplain org.fluidity.foundation.Log#trace(String, Object...) trace}: {@linkplain java.util.logging.Level#FINEST FINEST}</li>
+ * <li>{@linkplain org.fluidity.foundation.Log#debug(String, Object...) debug}: {@linkplain java.util.logging.Level#FINE FINE}</li>
+ * <li>{@linkplain org.fluidity.foundation.Log#info(String, Object...) info}: {@linkplain java.util.logging.Level#INFO INFO}</li>
+ * <li>{@linkplain org.fluidity.foundation.Log#warning(String, Object...) warning}: {@linkplain java.util.logging.Level#WARNING WARNING}</li>
+ * <li>{@linkplain org.fluidity.foundation.Log#error(String, Object...) error}: {@linkplain java.util.logging.Level#SEVERE SEVERE}</li>
+ * </ul>
+ *
+ * @author Tibor Varga
+ */
+@Component
+@ServiceProvider
+@SuppressWarnings("UnusedDeclaration")
+final class JavaLogFactory implements LogFactory {
+
+    private final LogManager loggers = LogManager.getLogManager();
+
+    /**
+     * {@inheritDoc}
+     */
+    public <T> Log<T> createLog(final Class<T> source) {
+        return new JavaLogImpl<T>(loggers, source);
+    }
+}
---
+++ features/logging-java-impl/src/main/java/org/fluidity/foundation/impl/JavaLogImpl.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2006-2012 Tibor Adam Varga (tibor.adam.varga on gmail)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fluidity.foundation.impl;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.fluidity.foundation.spi.AbstractLog;
+
+/**
+ * Uses <code>java.util.logging</code> as the underlying logging framework.
+ */
+final class JavaLogImpl<T> extends AbstractLog<Logger, T> {
+
+    JavaLogImpl(final LogManager loggers, final Class<?> source) {
+        super(loggers.getLogger(source.getName()), new Levels<Logger>() {
+            public boolean trace(final Logger log) {
+                return log.isLoggable(Level.FINEST);
+            }
+
+            public boolean debug(final Logger log) {
+                return log.isLoggable(Level.FINE);
+            }
+
+            public boolean info(final Logger log) {
+                return log.isLoggable(Level.INFO);
+            }
+
+            public boolean warning(final Logger log) {
+                return log.isLoggable(Level.WARNING);
+            }
+        });
+    }
+
+    public void trace(final String format, final Object... args) {
+        if (permissions.trace) {
+            log.log(Level.FINEST, String.format(format, args));
+        }
+    }
+
+    public void debug(final String format, final Object... args) {
+        if (permissions.debug) {
+            log.log(Level.FINE, String.format(format, args));
+        }
+    }
+
+    public void info(final String format, final Object... args) {
+        if (permissions.info) {
+            log.log(Level.INFO, String.format(format, args));
+        }
+    }
+
+    public void warning(final String format, final Object... args) {
+        if (permissions.info) {
+            log.log(Level.WARNING, String.format(format, args));
+        }
+    }
+
+    public void error(final String format, final Object... args) {
+        log.log(Level.SEVERE, String.format(format, args));
+    }
+
+    public void trace(final Throwable exception, final String format, final Object... args) {
+        if (permissions.trace) {
+            log.log(Level.FINEST, String.format(format, args), exception);
+        }
+    }
+
+    public void debug(final Throwable exception, final String format, final Object... args) {
+        if (permissions.debug) {
+            log.log(Level.FINE, String.format(format, args), exception);
+        }
+    }
+
+    public void info(final Throwable exception, final String format, final Object... args) {
+        if (permissions.info) {
+            log.log(Level.INFO, String.format(format, args), exception);
+        }
+    }
+
+    public void warning(final Throwable exception, final String format, final Object... args) {
+        if (permissions.info) {
+            log.log(Level.WARNING, String.format(format, args), exception);
+        }
+    }
+
+    public void error(final Throwable exception, final String format, final Object... args) {
+        log.log(Level.SEVERE, String.format(format, args), exception);
+    }
+}
